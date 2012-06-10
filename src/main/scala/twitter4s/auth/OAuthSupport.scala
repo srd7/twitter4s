95d83e318b45aed01c7c892b9537eaccb89869cc
Shinsuke Abe <mao.instantlife@gmail.com>
2012-06-10 22:02:35 +0900

    add comment setOAuthCustomer method

--- src/main/scala/twitter4s/auth/OAuthSupport.scala
+++ src/main/scala/twitter4s/auth/OAuthSupport.scala
@@ -2,7 +2,19 @@ package twitter4s.auth
 import twitter4j.auth.RequestToken
 import twitter4j.auth.AccessToken
 
+/**
+ * @author Shinsuke Abe - mao.instantlife at gmail.com
+ */
 trait OAuthSupport {
+  /**
+   * sets the OAuth consumer key and consumer secret
+   * <br/>This method call twitter4j.Twitter.setOAuthConsumer(consumerKey, consumerSecret)
+   *
+   * @param consumerKey OAuth consumer key
+   * @param consumerSecret OAuth consumer secret
+   * @throws IllegalStateException when OAuth consumer has already been set, or the instance is using basic authorization
+   * @since Twitter4S 1.0.0
+   */
   def setOAuthConsumer(consumerKey: String, consumerSecret: String): Unit
   
   def getOAuthRequestToken(callbackURL: Option[String], xAuthAccessType: Option[String]): RequestToken
