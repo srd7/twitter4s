bb069c9e8e98ea7f1d5f41b31ae7c7c133f656a8
Shinsuke Abe <mao.instantlife@gmail.com>
2012-06-16 15:10:39 +0900

    implements Twitter.screenName test.

--- src/test/scala/twitter4s/TwitterTest.scala
+++ src/test/scala/twitter4s/TwitterTest.scala
@@ -9,20 +9,26 @@ import twitter4s.Twitter4sTestHelper._
 
 @RunWith(classOf[JUnitRunner])
 class TwitterTest extends Specification {
-	"getAccessLevel" should {
-	  "unauthenticated access level is None" in {
-	    val response = unauthenticated.getDailyTrends()
-	    response.accessLevel must equalTo(TwitterResponse.NONE)
-	  }
-	  
-	  "application has read and write access level is READ_WRITE" in {
-	    val response = twitter1.verifyCredentials
-	    response.getAccessLevel() must equalTo(TwitterResponse.READ_WRITE)
-	  }
-	  
-	  "application has all access level is READ_WRITE_DIRECTMESSAGES" in {
-	    val response = rwPrivateMessage.verifyCredentials
-	    response.getAccessLevel() must equalTo(TwitterResponse.READ_WRITE_DIRECTMESSAGES)
-	  }
-	}
+  "getAccessLevel" should {
+    "unauthenticated access level is None" in {
+      val response = unauthenticated.getDailyTrends()
+      response.accessLevel must equalTo(TwitterResponse.NONE)
+    }
+  
+    "application has read and write access level is READ_WRITE" in {
+      val response = twitter1.verifyCredentials
+      response.getAccessLevel() must equalTo(TwitterResponse.READ_WRITE)
+    }
+  
+    "application has all access level is READ_WRITE_DIRECTMESSAGES" in {
+      val response = rwPrivateMessage.verifyCredentials
+      response.getAccessLevel() must equalTo(TwitterResponse.READ_WRITE_DIRECTMESSAGES)
+    }
+  }
+  
+  "screenName" should {
+    "get authorized user's screen name" in {
+      twitter1.screenName must equalTo(id1.screenName)
+    }
+  }
 }
