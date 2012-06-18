1dea21867e13010e83bd852fa2083e8e26158f9e
Shinsuke Abe <mao.instantlife@gmail.com>
2012-06-19 01:08:45 +0900

    implements GeoMethods API method.

--- src/main/scala/twitter4s/api/GeoMethods.scala
+++ src/main/scala/twitter4s/api/GeoMethods.scala
@@ -6,14 +6,70 @@ import twitter4j.Place
 import twitter4j.GeoLocation
 import twitter4j.SimilarPlaces
 
+/**
+ * @author Shinsuke Abe - mao.instantlife at gmail.com
+ */
 trait GeoMethods {
-	def searchPlaces(query: GeoQuery): ResponseList[Place]
-	
-	def getSimilarPlaces(location: GeoLocation, name: String, containedWithin: String, streetAddress: String): SimilarPlaces
-	
-	def reverseGeoCode(query: GeoQuery): ResponseList[Place]
-	
-	def getGeoDetails(id: String): Place
-	
-	def createPlace(name: String, containedWithin: String, token: String, location: GeoLocation, streetAddress: String): Place
+  /**
+   * Search for places that can be attached to a statuses/update. Given a latitude and a longitude pair, an IP address, or a name, this request will return a list of all the valid places that can be used as the place_id when updating a status.
+   * <br/>Conceptually, a query can be made from the user's location, retrieve a list of places, have the user validate the location he or she is at, and then send the ID of this location with a call to statuses/update.
+   * <br/>This is the recommended method to use find places that can be attached to statuses/update. Unlike geo/reverse_geocode which provides raw data access, this endpoint can potentially re-order places with regards to the user who is authenticated. This approach is also preferred for interactive place matching with the user.
+   * <br/>This method calls twitter4j.Twitter.searchPlaces(query)
+   * <br/>searchPlaces calls http://api.twitter.com/1/geo/search.json
+   *
+   * @param query search query
+   * @return places (cities and neighborhoods) that can be attached to a statuses/update
+   * @throws TwitterException when Twitter service or network is unavailable
+   * @see <a href="https://dev.twitter.com/docs/api/1/get/geo/search">GET geo/search | Twitter Developers</a>
+   * @since Twitter4S 1.0.0
+   */
+  def searchPlaces(query: GeoQuery): ResponseList[Place]
+  
+  /**
+   * Locates places near the given coordinates which are similar in name.
+   * <br/>Conceptually you would use this method to get a list of known places to choose from first. Then, if the desired place doesn't exist, make a request to post/geo/place to create a new one.
+   * <br/>The token contained in the response is the token needed to be able to create a new place.
+   * <br/>This method calls twitter4j.Twitter.getSimilarPlaces(location, name, containedWithin, streetAddress)
+   * <br/>getSimilarPlaces calls http://api.twitter.com/1/geo/similar_places.json
+   *
+   * @param location The latitude and longitude to search around.
+   * @param name The name a place is known as.
+   * @param containedWithin optional: the place_id which you would like to restrict the search results to. Setting this value means only places within the given place_id will be found.
+   * @param streetAddress optional: This parameter searches for places which have this given street address. There are other well-known, and application specific attributes available. Custom attributes are also permitted. Learn more about Place Attributes.
+   * @return places (cities and neighborhoods) that can be attached to a statuses/update
+   * @throws TwitterException when Twitter service or network is unavailable
+   * @since Twitter4S 1.0.0
+   */
+  def getSimilarPlaces(location: GeoLocation, name: String, containedWithin: String, streetAddress: String): SimilarPlaces
+  
+  /**
+   * Search for places (cities and neighborhoods) that can be attached to a statuses/update. Given a latitude and a longitude, return a list of all the valid places that can be used as a place_id when updating a status. Conceptually, a query can be made from the user's location, retrieve a list of places, have the user validate the location he or she is at, and then send the ID of this location up with a call to statuses/update.<br/>
+   * There are multiple granularities of places that can be returned -- "neighborhoods", "cities", etc. At this time, only United States data is available through this method.<br/>
+   * This API call is meant to be an informative call and will deliver generalized results about geography.
+   * <br/>This method calls twitter4j.Twitter.reverseGeoCode(query)
+   * <br/>This method calls http://api.twitter.com/1/geo/reverse_geocode.json
+   *
+   * @param query search query
+   * @return places (cities and neighborhoods) that can be attached to a statuses/update
+   * @throws TwitterException when Twitter service or network is unavailable
+   * @see <a href="https://dev.twitter.com/docs/api/1/get/geo/reverse_geocode">GET geo/reverse_geocode | Twitter Developers</a>
+   * @since Twitter4S 1.0.0
+   */
+  def reverseGeoCode(query: GeoQuery): ResponseList[Place]
+  
+  /**
+   * Find out more details of a place that was returned from the {@link twitter4j.api.GeoMethods#reverseGeoCode(twitter4j.GeoQuery)} method.
+   * <br/>This method calls twitter4j.Twitter.getGeoDetails(id)
+   * <br/>This method calls http://api.twitter.com/1/geo/id/:id.json
+   *
+   * @param id The ID of the location to query about.
+   * @return details of the specified place
+   * @throws TwitterException when Twitter service or network is unavailable
+   * @see <a href="https://dev.twitter.com/docs/api/1/get/geo/id/:place_id">GET geo/id/:place_id | Twitter Developers</a>
+   * @since Twitter4S 1.0.0
+   */
+  def getGeoDetails(id: String): Place
+  
+  // TODO コメント
+  def createPlace(name: String, containedWithin: String, token: String, location: GeoLocation, streetAddress: String): Place
 }
---
+++ src/test/scala/twitter4s/GeoMethodsTest.scala
@@ -0,0 +1,92 @@
+package twitter4s
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import twitter4s.Twitter4sTestHelper._
+import twitter4j.GeoQuery
+import twitter4j.GeoLocation
+import twitter4j.json.DataObjectFactory
+import twitter4j.Place
+import twitter4j.TwitterException
+import twitter4j.StatusUpdate
+import java.util.Date
+
+@RunWith(classOf[JUnitRunner])
+class GeoMethodsTest extends Specification {
+  
+  private def testPlaces(target: ResponseList[Place]) = {
+    rawJSON(target.twt4jResponseList) must not equalTo(null)
+    target(0) must equalTo(DataObjectFactory.createPlace(rawJSON(target(0))))
+    target().size must be_>(0)
+  }
+  
+  "reverseGeoCode" should {
+    "get no places if location is (0,0)" in {
+      val query = new GeoQuery(new GeoLocation(0, 0))
+      val places = twitter1.reverseGeoCode(query)
+      places().size must equalTo(0)
+    }
+    
+    "get place list if location is exists" in {
+      val query = new GeoQuery(new GeoLocation(37.78215, -122.40060))
+      testPlaces(twitter1.reverseGeoCode(query))
+    }
+  }
+  
+  "searchPlaces" should {
+    "get place list with location" in {
+      val query = new GeoQuery(new GeoLocation(37.78215, -122.40060))
+      testPlaces(twitter1.searchPlaces(query))
+    }
+  }
+  
+  "getSimilarPlaces" should {
+    "get similar place list with parameter location" in {
+      val places = twitter1.getSimilarPlaces(new GeoLocation(37.78215, -122.40060), "SoMa", null, null)
+      rawJSON(places) must not equalTo(null)
+      places.get(0) must equalTo(DataObjectFactory.createPlace(rawJSON(places.get(0))))
+      places.size() must be_>(0)
+    }
+  }
+  
+  "getGeoDetails" should {
+    "get detail location information" in {
+      try {
+        val place = unauthenticated.getGeoDetails("5a110d312052166f")
+        rawJSON(place) must not equalTo(null)
+        place must equalTo(DataObjectFactory.createPlace(rawJSON(place)))
+        place.getFullName() must equalTo("San Francisco, CA")
+        place.getContainedWithIn()(0).getFullName() must equalTo("California, US")
+      } catch {
+        // is being rate limited
+        case te: TwitterException => te.getStatusCode must equalTo(404)
+      }
+    }
+  }
+  
+  "updateStatus(StatusMethods)" should {
+    "update user status with place information" in {
+      val sanFrancisco = "5a110d312052166f"
+      val latestStatus = new StatusUpdate(new Date() + " status with place")
+      val status = twitter3.updateStatus(latestStatus = Some(latestStatus.placeId(sanFrancisco)))
+      rawJSON(status) must not equalTo(null)
+      status must equalTo(DataObjectFactory.createStatus(rawJSON(status)))
+      status.getPlace().getId() must equalTo(sanFrancisco)
+      status.getContributors() must equalTo(null)
+    }
+    
+    "update user status with geo information" in {
+      val latitude = 12.3456d
+      val longitude = -34.5678d
+      val latestStatus = new StatusUpdate(new Date() + " status with geo")
+      
+      val statusWithGeo = twitter3.updateStatus(latestStatus = Some(latestStatus.location(new GeoLocation(latitude, longitude))))
+      rawJSON(statusWithGeo) must not equalTo(null)
+      statusWithGeo must equalTo(DataObjectFactory.createStatus(rawJSON(statusWithGeo)))
+      statusWithGeo.getUser().isGeoEnabled() must beTrue
+      statusWithGeo.getGeoLocation().getLatitude() must equalTo(latitude)
+      statusWithGeo.getGeoLocation().getLongitude() must equalTo(longitude)
+      twitter1.verifyCredentials.isGeoEnabled() must beFalse
+    }
+  }
+}
