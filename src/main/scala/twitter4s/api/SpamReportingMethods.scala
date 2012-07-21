c280419ab2a9f1ec1fabaa8eea02682d00979ccf
Shinsuke Abe <mao.instantlife@gmail.com>
2012-07-21 17:50:46 +0900

    change SpamReportingMethods API comment.

--- src/main/scala/twitter4s/api/SpamReportingMethods.scala
+++ src/main/scala/twitter4s/api/SpamReportingMethods.scala
@@ -8,13 +8,16 @@ import twitter4j.User
 trait SpamReportingMethods {
   /**
    * The user specified in the id is blocked by the authenticated user and reported as a spammer.
-   * <br/>This method calls twitter4j.Twitter.reportSpam(userId) or reportSpam(screenName)
-   * <br/>This method calls http://api.twitter.com/1/report_spam.json
+   * <br/>This method calls twitter4j.Twitter.reportSpam.
+   * <br/>reportSpam calls http://api.twitter.com/1/report_spam.json
+   * <br/>Note1: You must set userId or screenName at least.
+   * <br/>Note2: Parameter userId is taken priority over screenName.
    *
    * @param userId (optional) The ID of the user you want to report as a spammer.
    * @param screenName (optional) The screen name of the user you want to report as a spammer.
    * @return The User reported as a spammer.
    * @throws TwitterException when Twitter service or network is unavailable
+   * @throws IllegalArgumentException when both userId and screenName are nost set.
    * @see <a href="https://dev.twitter.com/docs/api/1/post/report_spam">POST report_spam | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
