3a71e75e375437a9358874b1fe9106d22b1f99e1
Shinsuke Abe <mao.instantlife@gmail.com>
2012-07-21 15:41:00 +0900

    change NotificationMethods APIs comments.

--- src/main/scala/twitter4s/api/NewTwitterMethods.scala
+++ src/main/scala/twitter4s/api/NewTwitterMethods.scala
@@ -9,7 +9,7 @@ trait NewTwitterMethods {
   
   /**
    * If available, returns an array of replies and mentions related to the specified Tweet. There is no guarantee there will be any replies or mentions in the response. This method is only available to users who have access to #newtwitter.
-   * <br/>This method calls twitter4j.Twitter.getRelatedResults(statusId)
+   * <br/>This method calls twitter4j.Twitter.getRelatedResults.
    * <br/>getRelatedResults has not been finalized and the interface is subject to change in incompatible ways.
    * <br/>getRelatedResults calls http://api.twitter.com/1/related_results/show/:id
    *
--- src/main/scala/twitter4s/api/NotificationMethods.scala
+++ src/main/scala/twitter4s/api/NotificationMethods.scala
@@ -5,13 +5,16 @@ import twitter4j.User
 trait NotificationMethods {
   /**
    * Enables notifications for updates from the specified user to the authenticating user. Returns the specified user when successful.
-   * <br/>This method calls twitter4j.Twitter.enableNotification(screenName) or enableNotification(userId)
-   * <br>enableNotification calls http://api.twitter.com/1/notifications/follow.json
+   * <br/>This method calls twitter4j.Twitter.enableNotification.
+   * <br/>enableNotification calls http://api.twitter.com/1/notifications/follow.json
+   * <br/>Note1: You must set screenName or userId at least.
+   * <br/>Note2: Parameter userId is taken priority over screenName.
    *
    * @param screenName (optional) Specifies the screen name of the user to follow with device updates.
    * @param userId (optional) Specifies the ID of the user to follow with device updates.
    * @return User
    * @throws TwitterException when Twitter service or network is unavailable
+   * @throws IllegalArgumentException when both of screenName and userId are not set.
    * @see <a href="https://dev.twitter.com/docs/api/1/post/notifications/follow">POST notifications/follow | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
@@ -19,13 +22,16 @@ trait NotificationMethods {
 
   /**
    * Disables notifications for updates from the specified user to the authenticating user. Returns the specified user when successful.
-   * <br/>This method calls twitter4j.Twitter.disableNotification(screenName) or disableNotification(userId).
+   * <br/>This method calls twitter4j.Twitter.disableNotification
    * <br/>disableNotification calls http://api.twitter.com/1/notifications/leave.json
+   * <br/>Note1: You must set screenName or userId at least.
+   * <br/>Note2: Parameter userId is taken priority over screenName.
    *
    * @param screenName (optional) Specifies the screen name of the user to disable device notifications.
    * @param userId (optional) Specifies the ID of the user to disable device notifications.
    * @return User
    * @throws TwitterException when Twitter service or network is unavailable
+   * @throws IllegalArgumentException when both of screenName and userId are not set.
    * @see <a href="https://dev.twitter.com/docs/api/1/post/notifications/leave">POST notifications/leave | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
