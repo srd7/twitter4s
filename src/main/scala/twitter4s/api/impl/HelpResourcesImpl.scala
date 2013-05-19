d22c0c71dae8ace872ccd5f538b1895e10c27a43
Shinsuke Abe <mao.instantlife@gmail.com>
2013-05-19 19:28:09 +0900

    add header comment to HelpResourceImpl.

--- src/main/scala/twitter4s/api/impl/HelpResourcesImpl.scala
+++ src/main/scala/twitter4s/api/impl/HelpResourcesImpl.scala
@@ -1,10 +1,27 @@
 package twitter4s.api.impl
-
+/*
+ * Copyright (C) 2013 Shinsuke Abe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import twitter4s._
 import api.HelpResources
 import twitter4j.api.HelpResources.Language
 import api.impl._
 
+/**
+ * @author Shinsuke Abe - mao.instantlife at gmail.com
+ */
 trait HelpResourcesImpl extends HelpResources {
   self: Twitter => 
   
