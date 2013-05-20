b583609293deeed897df451aaa5b153c150fe206
Shinsuke Abe <mao.instantlife@gmail.com>
2013-05-20 22:53:09 +0900

    add header comment.

--- src/main/scala/twitter4s/api/impl/PlaceGeoResourcesImpl.scala
+++ src/main/scala/twitter4s/api/impl/PlaceGeoResourcesImpl.scala
@@ -1,10 +1,28 @@
 package twitter4s.api.impl
-
+/*
+ * Copyright (C) 2013 Shinsuke Abe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import twitter4s._
 import twitter4s.ResponseList
 import twitter4j.GeoQuery
 import twitter4s.api.PlaceGeoResources
 
+/**
+ * @author Shinsuke Abe - mao.instantlife at gmail.com
+ * @since Twitter4S 2.0.0
+ */
 trait PlaceGeoResourcesImpl extends PlaceGeoResources {
   self: Twitter =>
   
