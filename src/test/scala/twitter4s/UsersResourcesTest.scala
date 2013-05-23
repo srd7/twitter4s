06cd83768f219e39dac5ad7fc33edb42a5fcc030
Shinsuke Abe <mao.instantlife@gmail.com>
2013-05-23 23:18:11 +0900

    merge AccontMethodTest and BlockMethodsTest to UsersREsourcesTest.

--- src/test/scala/twitter4s/UsersResourcesTest.scala
+++ src/test/scala/twitter4s/UsersResourcesTest.scala
@@ -7,6 +7,9 @@ import org.specs2.runner.JUnitRunner
 import twitter4j.json.DataObjectFactory
 import Twitter4sTestHelper._
 import internal.json.ResponseListImpl
+import java.io.FileInputStream
+import java.util.Date
+import java.io.File
 
 @RunWith(classOf[JUnitRunner])
 class UsersResourcesTest extends Specification {
@@ -148,4 +151,217 @@ class UsersResourcesTest extends Specification {
 //      image.getURL must not equalTo(null)
 //    }
 //  }
+    "verifyCredentials" should {
+    "get authorized user's credentials information" in {
+      val original = twitter1.verifyCredentials
+      rawJSON(original.tw4jObj) must not equalTo(null)
+      original.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(original.tw4jObj)))
+    }
+  }
+  
+  "updateProfile" should {
+    "update user profile(name, url, location, description)" in {
+      // make precondition
+      val original = twitter3.updateProfile(
+          "twt4s_id3",
+          "https://github.com/Shinsuke-Abe/twitter4s",
+          ":Location",
+          "Hi there, I do test a lot!new")
+      
+      val newName = original.name + "new"
+      val newURL = original.url + "new"
+      val newLocation = new Date().toString()
+      val newDescription = original.description + "new"
+      
+      // test
+      val altered = twitter3.updateProfile(newName, newURL, newLocation, newDescription)
+      rawJSON(altered.tw4jObj) must not equalTo(null)
+      original.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(original.tw4jObj)))
+      altered.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(altered.tw4jObj)))
+      altered.name must equalTo(newName)
+      altered.url.toString() must equalTo(newURL)
+      altered.location must equalTo(newLocation)
+      altered.description must equalTo(newDescription)
+    }
+  }
+  
+  "updateProfileColors" should {
+    def testProfileColors(
+        target: User,
+        profileBackgroundColor: String,
+        profileTextColor: String,
+        profileLinkColor: String,
+        profileSidebarFillColor: String,
+        profileSidebarBorderColor: String) = {
+      target.profileBackgroundColor must equalTo(profileBackgroundColor)
+      target.profileTextColor must equalTo(profileTextColor)
+      target.profileLinkColor must equalTo(profileLinkColor)
+      target.profileSidebarFillColor must equalTo(profileSidebarFillColor)
+      target.profileSidebarBorderColor must equalTo(profileSidebarBorderColor)
+    }
+    
+    "change colors on user status page with three characters" in {
+      val updatedUser = twitter3.updateProfileColors("f00", "f0f", "0ff", "0f0", "f0f")
+      rawJSON(updatedUser.tw4jObj) must not equalTo(null)
+      updatedUser.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(updatedUser.tw4jObj)))
+      testProfileColors(updatedUser, "f00", "f0f", "0ff", "0f0", "f0f")
+      updatedUser.isProfileUseBackgroundImage must beTrue
+      updatedUser.isShowAllInlineMedia must beFalse
+      updatedUser.listedCount must be_<=(0)
+    }
+    
+    "change colors on user status page with six characters" in {
+      val updatedUser = twitter3.updateProfileColors("87bc44", "9ae4e8", "000000", "0000ff", "e0ff92")
+      rawJSON(updatedUser.tw4jObj) must not equalTo(null)
+      updatedUser.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(updatedUser.tw4jObj)))
+      testProfileColors(updatedUser, "87bc44", "9ae4e8", "000000", "0000ff", "e0ff92")
+    }
+  }
+  
+  "getAccountTotals" should {
+    "get user's (favorite, followers, friends, udpates) total number" in {
+      val totals = twitter1.getAccountTotals
+      totals.favorites must be_>(0)
+      totals.followers must be_>(0)
+      totals.friends must be_>(0)
+      totals.updates must be_>(0)
+      totals.tw4jObj must equalTo(DataObjectFactory.createAccountTotals(rawJSON(totals.tw4jObj)))
+    }
+  }
+  
+  "getAccountSettings" should {
+    "get authorized user's account settings" in {
+      val settings = twitter1.getAccountSettings
+      settings.isSleepTimeEnabled must beTrue // this setting's default is false
+      settings.isGeoEnabled must beFalse // this setting's default is false
+      settings.language must equalTo("ja")
+      settings.timeZone.getName() must equalTo("Osaka")
+      settings.isAlwaysUseHttps must beTrue
+      settings.isDiscoverableByEmail must beTrue
+      settings.trendLocations.length must be_>(0)
+    }
+  }
+  
+  "updateAccountSettings" should {
+    "change authorized user's account settings" in {
+      val intermSetting = twitter3.updateAccountSettings(1, true, "23", "08", "Helsinki", "it")
+      rawJSON(intermSetting.tw4jObj) must not equalTo(null)
+      intermSetting.sleepStartTime must equalTo("23")
+      intermSetting.sleepEndTime must equalTo("8")
+      intermSetting.isGeoEnabled must beTrue // is default false
+      intermSetting.language must equalTo("it")
+      intermSetting.isAlwaysUseHttps must beTrue
+      intermSetting.isDiscoverableByEmail must beTrue // is default
+      intermSetting.timeZone.getName() must equalTo("Helsinki")
+      intermSetting.trendLocations.length must be_>(0)
+    }
+  }
+  
+  "updateProfileImage" should {
+    "change profile image" in {
+      val user = twitter2.updateProfileImage(
+          ImageResource.isAssigned(new FileInputStream(getRandomlyChosenFile)))
+      rawJSON(user.tw4jObj) must not equalTo(null)
+    }
+    
+    "throws IllegalArgumentException with no image file and stream" in {
+       twitter2.updateProfileImage(null) must throwA[IllegalArgumentException]
+    }
+  }
+  
+  "updateProfileBackgroundImage" should {
+    "change background image in authorized user page" in {
+      val user = twitter2.updateProfileBackgroundImage(
+          ImageResource.isAssigned(getRandomlyChosenFile),
+          (5 < System.currentTimeMillis() % 5))
+      rawJSON(user.tw4jObj) must not equalTo(null)
+      user.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(user.tw4jObj)))
+    }
+    
+    "throws IllegalArgumentException with no image file and stream" in {
+      twitter2.updateProfileBackgroundImage(null, true) must throwA[IllegalArgumentException]
+    }
+  }
+  
+  val imageFiles = Array(
+      "src/test/resources/t4j-reverse.jpeg",
+      "src/test/resources/t4j-reverse.png",
+      "src/test/resources/t4j-reverse.gif",
+      "src/test/resources/t4j.jpeg",
+      "src/test/resources/t4j.png",
+      "src/test/resources/t4j.gif"
+      )
+  
+  def getRandomlyChosenFile = {
+    new File(imageFiles((System.currentTimeMillis() % 6).toInt))
+  }
+  
+    private def testBlockingUsers(target: ResponseList[twitter4j.User]) = {
+    rawJSON(target.tw4jObj) must not equalTo(null)
+    target(0) must equalTo(DataObjectFactory.createUser(rawJSON(target(0))))
+    target.size must equalTo(blockingUsersSize)
+    target(0).id must equalTo(blockingUserId)
+  }
+  
+  "createBlock" should {
+    "create block and get blocked user" in {
+      val user = twitter2.createBlock(
+          User.isSpecifiedBy(id3.id))
+      rawJSON(user.tw4jObj) must not equalTo(null)
+      user.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(user.tw4jObj)))
+    }
+    
+    "throw exception when user specific info is set null" in {
+      twitter2.createBlock(null) must
+      throwA[IllegalArgumentException]
+    }
+  }
+  
+  "destryoBlock" should {
+    "destroy block and get unblocked user" in {
+      val user = twitter2.destroyBlock(
+          User.isSpecifiedBy(id3.screenName))
+      rawJSON(user.tw4jObj) must not equalTo(null)
+      user.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(user.tw4jObj)))
+      
+      // check destroyed block
+      twitter3.existsBlock(
+          User.isSpecifiedBy(id2.id)) must beFalse
+      // check blocked user
+      val blockedUserScreenName = blockingScreenName
+      twitter1.existsBlock(
+          User.isSpecifiedBy(blockedUserScreenName)) must beTrue
+    }
+    
+    "throw exception when user specific info is set null" in {
+      twitter2.destroyBlock(null) must
+      throwA[IllegalArgumentException] 
+    }
+  }
+  
+  "existBlock" should {
+    "thorw exception both of screenName and userId are not set" in {
+      twitter2.existsBlock(null) must
+      throwA[IllegalArgumentException]
+    }
+  }
+  
+  "getBlockingUsers" should {
+    "get all user list blocking by authorized user" in {
+      testBlockingUsers(twitter1.getBlockingUsers())
+    }
+    
+    "get user list specified page blocking by authorized user" in {
+      testBlockingUsers(twitter1.getBlockingUsers(1))
+    }
+  }
+  
+  "getBlockingUsersIDs" should {
+    "get user id list blocking by authorized user" in {
+      val ids = twitter1.getBlockingUsersIDs
+      rawJSON(ids.tw4jObj) must not equalTo(null)
+      ids.ids.size must equalTo(blockingUsersSize)
+      ids.ids(0) must equalTo(blockingUserId)
+    }
+  }
 }
