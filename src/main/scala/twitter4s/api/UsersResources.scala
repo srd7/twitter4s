2e90bb9f75f3e72f77e96ee4c8ea3df5a135d4b4
Shinsuke Abe <mao.instantlife@gmail.com>
2013-07-15 09:25:18 +0900

    update comment for api 1.1

--- src/main/scala/twitter4s/api/UsersResources.scala
+++ src/main/scala/twitter4s/api/UsersResources.scala
@@ -31,11 +31,11 @@ trait UsersResources {
     /**
    * Returns an HTTP 200 OK response code and a representation of the requesting user if authentication was successful; returns a 401 status code and an error message if not. Use this method to test if supplied user credentials are valid.
    * <br />This method calls twitter4j.Twitter.verifyCredentials.
-   * <br />verifyCredentials calls http://api.twitter.com/1/account/verify_credentials.json
+   * <br />verifyCredentials calls http://api.twitter.com/1.1/account/verify_credentials.json
    * 
    * @return user
    * @throws twitter4j.TwitterException when Twitter service or network is unavailable, or if supplied credential is wrong (TwitterException.getStatusCode() == 401)
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/account/verify_credentials">GET account/verify_credentials | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/account/verify_credentials">GET account/verify_credentials | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def verifyCredentials: User
@@ -43,7 +43,7 @@ trait UsersResources {
   /**
    * Sets one or more hex values that control the color scheme of the authenticating user's profile page on twitter.com. Each parameter's value must be a valid hexidecimal value, and may be either three or six characters (ex: #fff or #ffffff).<br/>
    * <br />This method calls twitter4j.Twitter.updateProfileColors.
-   * <br />updateProfileColors calls http://api.twitter.com/1/account/update_profile_colors.json
+   * <br />updateProfileColors calls http://api.twitter.com/1.1/account/update_profile_colors.json
    * 
    * @param profileBackgroundColor optional, can be null
    * @param profileTextColor optional, can be null
@@ -52,7 +52,7 @@ trait UsersResources {
    * @param profileSidebarBorderColor optional, can be null
    * @return the updated user
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/account/update_profile_colors">POST account/update_profile_colors | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/account/update_profile_colors">POST account/update_profile_colors | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def updateProfileColors(
@@ -65,7 +65,7 @@ trait UsersResources {
   /**
    * Updates the authenticating user's profile image.<br/>
    * <br />This method calls twitter4j.Twitter.updateProfileImage.
-   * <br />updateProfileImage calls http://api.twitter.com/1/account/update_profile_image.json
+   * <br />updateProfileImage calls http://api.twitter.com/1.1/account/update_profile_image.json
    * <br />Note: You must set parameter imageFile or imageStream at least.
    * <br />Profile image must be a valid GIF, JPG, or PNG image of less than 700 kilobytes in size. Images with width larger than 500 pixels will be scaled down.
    *
@@ -74,7 +74,7 @@ trait UsersResources {
    * @throws TwitterException when Twitter service or network is unavailable,
    * or when the specified file is not found (FileNotFoundException will be nested),
    * or when the specified file object in not representing a file (IOException will be nested).
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/account/update_profile_image">POST account/update_profile_image | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/account/update_profile_image">POST account/update_profile_image | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def updateProfileImage(imageResource: ImageResource.SpecificResource): User
@@ -82,7 +82,7 @@ trait UsersResources {
   /**
    * Updates the authenticating user's profile background image.<br/>
    * <br />This method calls twitter4.Twitter.updateProfileBackgroundImage.
-   * <br />updateProfileBackgroundImage method calls http://api.twitter.com/1/account/update_profile_background_image.json
+   * <br />updateProfileBackgroundImage method calls http://api.twitter.com/1.1/account/update_profile_background_image.json
    * <br />Note: You must set prameter imageFile or imageStream at least.
    * <br />Background image must be a valid GIF, JPG, or PNG image of less than 800 kilobytes in size. Images with width larger than 2048 pixels will be forceably scaled down.
    *
@@ -92,7 +92,7 @@ trait UsersResources {
    * @throws TwitterException when Twitter service or network is unavailable,
    * or when the specified file is not found (FileNotFoundException will be nested),
    * or when the specified file object in not representing a file (IOException will be nested)
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/account/update_profile_background_image">POST account/update_profile_background_image | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/account/update_profile_background_image">POST account/update_profile_background_image | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def updateProfileBackgroundImage(
@@ -102,7 +102,7 @@ trait UsersResources {
   /**
    * Sets values that users are able to set under the "Account" tab of their settings page. Only the parameters specified(non-null) will be updated.<br/>
    * <br />This method calls twitter4j.Twitter.updateProfile.
-   * <br />updateProfile calls http://api.twitter.com/1/account/update_profile.json
+   * <br />updateProfile calls http://api.twitter.com/1.1/account/update_profile.json
    *
    * @param name Optional. Maximum of 20 characters.
    * @param url Optional. Maximum of 100 characters. Will be prepended with "http://" if not present.
@@ -110,7 +110,7 @@ trait UsersResources {
    * @param description Optional. Maximum of 160 characters.
    * @return the updated user
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/account/update_profile">POST account/update_profile | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/account/update_profile">POST account/update_profile | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def updateProfile(
@@ -123,11 +123,11 @@ trait UsersResources {
    * Returns the current trend, geo, language, timezone and sleep time information for the authenticating user.
    * <br />This method has not been finalized and the interface is subject to change in incompatible ways.
    * <br />This method calls twitter4j.Twitter.getAccountSettings.
-   * <br />getAccountSettings calls http://api.twitter.com/1/account/settings.json
+   * <br />getAccountSettings calls http://api.twitter.com/1.1/account/settings.json
    *
    * @return the current trend, geo and sleep time information for the authenticating user.
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/account/settings">GET account/settings | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/account/settings">GET account/settings | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def getAccountSettings: AccountSettings
@@ -136,7 +136,7 @@ trait UsersResources {
    * Updates the current trend, geo, language, timezone and sleep time information for the authenticating user.
    * <br />This method has not been finalized and the interface is subject to change in incompatible ways.
    * <br />This method calls twitter4j.Twitter.updateAccountSettings.
-   * <br />updateAccountSettings calls http://api.twitter.com/1/account/settings.json
+   * <br />updateAccountSettings calls http://api.twitter.com/1.1/account/settings.json
    *
    * @param trendLocationWoeid Optional. The Yahoo! Where On Earth ID to use as the user's default trend location.
    * @param sleepTimeEnabled Optional. Whether sleep time is enabled for the user
@@ -146,7 +146,7 @@ trait UsersResources {
    * @param lang Optional. The language which Twitter should render in for this user. (two letter ISO 639-1)
    * @return the current trend, geo and sleep time information for the authenticating user.
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/account/settings">POST account/settings | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/account/settings">POST account/settings | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def updateAccountSettings(
@@ -160,7 +160,7 @@ trait UsersResources {
   /**
    * Blocks the user specified in the ID parameter as the authenticating user. Returns the blocked user in the requested format when successful.
    * <br />This method calls twitter4j.Twitter.createBlock(screenName) or createBlock(userId)
-   * <br />createBlock calls http://api.twitter.com/1/blocks/create/[id].json
+   * <br />createBlock calls http://api.twitter.com/1.1/blocks/create/[id].json
    * <br />Note1: You must set parameter screenName or userId at least.
    * <br />Note2: Parameter userId is taken priority over screenName.
    *
@@ -168,7 +168,7 @@ trait UsersResources {
    * @return the blocked user
    * @throws TwitterException when Twitter service or network is unavailable
    * @throws IllegalArgumentException when specificUser is set null.
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/blocks/create">POST blocks/create | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/blocks/create">POST blocks/create | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def createBlock(specificUser: User.SpecificInfo): User
@@ -176,7 +176,7 @@ trait UsersResources {
   /**
    * Un-blocks the user specified in the ID parameter as the authenticating user. Returns the un-blocked user in the requested format when successful.
    * <br />This method calls twitter4j.Twitter.destroyBlock(screenName) or destroyBlock(userId)
-   * <br />destroyBlock calls http://api.twitter.com/1/blocks/destroy/[id].json
+   * <br />destroyBlock calls http://api.twitter.com/1.1/blocks/destroy/[id].json
    * <br />Note1: You must set parameter screenName or userId at least.
    * <br />Note2: Parameter userId is taken priority over screenName.
    *
@@ -184,7 +184,7 @@ trait UsersResources {
    * @return the unblocked user
    * @throws TwitterException when Twitter service or network is unavailable
    * @throws IllegalArgumentException when specificUser is set null.
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/blocks/destroy">POST blocks/destroy | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/blocks/destroy">POST blocks/destroy | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def destroyBlock(specificUser: User.SpecificInfo): User
@@ -192,12 +192,12 @@ trait UsersResources {
   /**
    * Returns a list of user objects that the authenticating user is blocking.
    * <br />This method calls twitter4j.Twitter.getBlocksList() or getBlocksList(page)
-   * <br />getBlockingUsers calls http://api.twitter.com/1/blocks/blocking.json
+   * <br />getBlockingUsers calls http://api.twitter.com/1.1/blocks/blocking.json
    *
    * @param page (optional) the number of page
    * @return a list of user objects that the authenticating user
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/blocks/blocking">GET blocks/blocking | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/blocks/blocking">GET blocks/blocking | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def getBlocksList(cursor: java.lang.Long = null): ResponseList[twitter4j.User]
@@ -205,11 +205,11 @@ trait UsersResources {
   /**
    * Returns an array of numeric user ids the authenticating user is blocking.
    * <br />This method calls twitter4j.Twitter.getBlockingUsersIDs
-   * <br />getBlockingUsersIDs calls http://api.twitter.com/1/blocks/blocking/ids
+   * <br />getBlockingUsersIDs calls http://api.twitter.com/1.1/blocks/ids
    *
    * @return Returns an array of numeric user ids the authenticating user is blocking.
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/blocks/blocking/ids">GET blocks/blocking/ids | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/blocks/ids">GET blocks/blocking/ids | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def getBlocksIDs(cursor: java.lang.Long = null): IDs
@@ -217,7 +217,7 @@ trait UsersResources {
   /**
    * Returns extended information of a given user, specified by ID or screen name as per the required id parameter. The author's most recent status will be returned inline.
    * <br />This method calls twitter4j.Twitter.showUser.
-   * <br />showUser calls http://api.twitter.com/1/users/show.json
+   * <br />showUser calls http://api.twitter.com/1.1/users/show.json
    * <br />Note1: You must set screenName or userId at least.
    * <br />Note2: Parameter userId is taken priority over screenName.
    *
@@ -225,7 +225,7 @@ trait UsersResources {
    * @return users
    * @throws TwitterException when Twitter service or network is unavailable
    * @throws IllegalArgumentException when specificUser is set null.
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/users/show">GET users/show | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/users/show">GET users/show | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def showUser(specificUser: User.SpecificInfo): User
@@ -233,14 +233,14 @@ trait UsersResources {
   /**
    * Return up to 100 users worth of extended information, specified by either ID, screen name, or combination of the two. The author's most recent status (if the authenticating user has permission) will be returned inline.
    * <br />This method calls twitter4j.Twitter.lookupUsers.
-   * <br />lookupUsers calls http://api.twitter.com/1/users/lookup.json
+   * <br />lookupUsers calls http://api.twitter.com/1.1/users/lookup.json
    * <br />Note1: You must set screenNames or ids at least.
    * <br />Note2: Parameter ids is taken priority over screenNames.
    *
    * @param specificUsers (optional) user specific information(screen names or ids) to return
    * @return users
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/users/lookup">GET users/lookup | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/users/lookup">GET users/lookup | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def lookupUsers(specificUsers: Users.SpecificInfo): ResponseList[twitter4j.User]
@@ -249,13 +249,13 @@ trait UsersResources {
    * Run a search for users similar to the Find People button on Twitter.com; the same results returned by people search on Twitter.com will be returned by using this API.<br />
    * Usage note: It is only possible to retrieve the first 1000 matches from this API.
    * <br />This method calls twitter4j.Twitter.searchUsers.
-   * <br />searchUsers calls http://api.twitter.com/1/users/search.json
+   * <br />searchUsers calls http://api.twitter.com/1.1/users/search.json
    *
    * @param query The query to run against people search.
    * @param page Specifies the page of results to retrieve. Number of statuses per page is fixed to 20.
    * @return the list of Users matches the provided
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/users/search">GET users/search | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/users/search">GET users/search | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def searchUsers(query: String, page: Int): ResponseList[twitter4j.User]
