4c251184ee9828fbb2e133a65435814d189de0ce
Shinsuke Abe <mao.instantlife@gmail.com>
2013-07-15 10:56:48 +0900

    update comment for api 1.1

--- src/main/scala/twitter4s/api/DirectMessagesResources.scala
+++ src/main/scala/twitter4s/api/DirectMessagesResources.scala
@@ -26,24 +26,24 @@ trait DirectMessagesResources {
   /**
    * Returns a list of the direct messages sent to the authenticating user.
    * <br />This method calls twitter4j.Twitter.getDirectMessages
-   * <br />getDirectMessages calls http://api.twitter.com/1/direct_messages
+   * <br />getDirectMessages calls http://api.twitter.com/1.1/direct_messages
    *
    * @param paging (optional) controls pagination. Supports since_id, max_id, count and page parameters.
    * @return List
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/direct_messages">GET direct_messages | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/direct_messages">GET direct_messages | Twitter Developers</a>
    */
   def getDirectMessages(paging: Paging = null): ResponseList[twitter4j.DirectMessage]
 
   /**
    * Returns a list of the direct messages sent by the authenticating user.
    * <br />This method calls twitter4j.Twitter.getSentDirectMessages
-   * <br />getSentDirectMessages calls http://api.twitter.com/1/direct_messages/sent
+   * <br />getSentDirectMessages calls http://api.twitter.com/1.1/direct_messages/sent
    *
    * @param paging (optional) controls pagination. Supports since_id, max_id, count and page parameters.
    * @return List
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/direct_messages/sent">GET direct_messages/sent | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/direct_messages/sent">GET direct_messages/sent | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def getSentDirectMessages(paging: Paging = null): ResponseList[twitter4j.DirectMessage]
@@ -52,7 +52,7 @@ trait DirectMessagesResources {
    * Sends a new direct message to the specified user from the authenticating user. Requires both the user and text parameters below.
    * The text will be trimmed if the length of the text is exceeding 140 characters.
    * <br />This method calls twitter4j.Twitter.sendDirectMessage
-   * <br />sendDirectMessage calls http://api.twitter.com/1/direct_messages/new
+   * <br />sendDirectMessage calls http://api.twitter.com/1.1/direct_messages/new
    * <br />Note1: You must set parameter screenName or userId at least.
    * <br />Note2: Parameter userId is taken priority over screenName.
    *
@@ -61,7 +61,7 @@ trait DirectMessagesResources {
    * @return DirectMessage
    * @throws TwitterException when Twitter service or network is unavailable
    * @throws IllegalArgumentException when specificUser is set null.
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/direct_messages/new">POST direct_messages/new | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/direct_messages/new">POST direct_messages/new | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def sendDirectMessage(specificUser: User.SpecificInfo, text: String): DirectMessage
@@ -69,12 +69,12 @@ trait DirectMessagesResources {
   /**
    * Destroys the direct message specified in the required ID parameter. The authenticating user must be the recipient of the specified direct message.
    * <br />This method calls twitter4j.Twitter.destroyDirectMessage.
-   * <br />destroyDirectMessage calls http://api.twitter.com/1/direct_messages/destroy
+   * <br />destroyDirectMessage calls http://api.twitter.com/1.1/direct_messages/destroy
    *
    * @param id the ID of the direct message to destroy
    * @return the deleted direct message
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/direct_messages/destroy/:id">POST direct_messages/destroy/:id | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/direct_messages/destroy/:id">POST direct_messages/destroy/:id | Twitter Developers</a>
    * @since Twitter4J 1.0.0
    */
   def destroyDirectMessage(id: Long): DirectMessage
@@ -83,7 +83,7 @@ trait DirectMessagesResources {
    * Returns a single direct message, specified by an id parameter.
    * <br />This method has not been finalized and the interface is subject to change in incompatible ways.
    * <br />This method calls twitter4j.Twitter.showDirectMessage.
-   * <br />showDirectMessage calls http://api.twitter.com/1/direct_messages/show/:id.json
+   * <br />showDirectMessage calls http://api.twitter.com/1.1/direct_messages/show/:id.json
    *
    * @param id message id
    * @return DirectMessage
