d27d9cba6a4e2383c4bb72d0fa74ed4974ae6364
Shinsuke Abe <mao.instantlife@gmail.com>
2013-07-16 20:48:18 +0900

    update comment.

--- src/main/scala/twitter4s/api/FriendsFollowersResources.scala
+++ src/main/scala/twitter4s/api/FriendsFollowersResources.scala
@@ -29,7 +29,7 @@ trait FriendsFollowersResources {
   /**
    * Returns an array of numeric IDs for every user the authenticating user is following.
    * <br />This method calls twitter4j.Twitter.getFriendsIDs.
-   * <br />getFriendsIDs calls http://api.twitter.com/1/friends/ids.json
+   * <br />getFriendsIDs calls http://api.twitter.com/1.1/friends/ids.json
    * <br />Note: Parameter userId is taken priority over screenName.
    *
    * @param cursor (required) Causes the list of connections to be broken into pages of no more than 5000 IDs at a time. The number of IDs returned is not guaranteed to be 5000 as suspended users are filterd out after connections are queried. <br/>
@@ -37,7 +37,7 @@ trait FriendsFollowersResources {
    * @param specificUser (optional) Specifies the user (screen name or ID) for whom to return the friends list.
    * @return an array of numeric IDs for every user the authenticating user is following
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/friends/ids">GET friends/ids | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/friends/ids">GET friends/ids | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def getFriendsIDs(cursor: Long, specificUser: User.SpecificInfo = null): IDs
@@ -45,7 +45,7 @@ trait FriendsFollowersResources {
   /**
    * Returns an array of numeric IDs for every user the specified user is followed by.
    * <br />This method calls twitter4j.Twitter.getFollowersIDs.
-   * <br />getFollowersIDs calls http://api.twitter.com/1/followers/ids.json
+   * <br />getFollowersIDs calls http://api.twitter.com/1.1/followers/ids.json
    * <br />Note: Parameter userId is taken priority over screenName.
    *
    * @param cursor (required) Causes the list of connections to be broken into pages of no more than 5000 IDs at a time. The number of IDs returned is not guaranteed to be 5000 as suspended users are filterd out after connections are queried. <br/>
@@ -53,7 +53,7 @@ trait FriendsFollowersResources {
    * @param specificUser (optional) Specifies the user (screen name or ID) for whom to return the followers list.
    * @return The ID or screen_name of the user to retrieve the friends ID list for.
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/followers/ids">GET followers/ids | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/followers/ids">GET followers/ids | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def getFollowersIDs(cursor: Long, specificUser: User.SpecificInfo = null): IDs
@@ -62,7 +62,7 @@ trait FriendsFollowersResources {
    * Allows the authenticating users to follow the user specified in the ID parameter.<br />
    * Returns the befriended user in the requested format when successful. Returns a string describing the failure condition when unsuccessful. If you are already friends with the user an HTTP 403 will be returned.
    * <br />This method calls twitter4j.Twitter.createFriendship.
-   * <br />createFriendship calls http://api.twitter.com/1/friendships/create/[id].json
+   * <br />createFriendship calls http://api.twitter.com/1.1/friendships/create/[id].json
    * <br />Note1: You must set parameter screenName or userId at least.
    * <br />Note2: Parameter userId is taken priority over screenName.
    *
@@ -71,7 +71,7 @@ trait FriendsFollowersResources {
    * @return the befriended user
    * @throws TwitterException when Twitter service or network is unavailable
    * @throws IllegalArgumentException when specificUser is set null.
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/friendships/create">POST friendships/create | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/friendships/create">POST friendships/create | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def createFriendship(
@@ -82,7 +82,7 @@ trait FriendsFollowersResources {
    * Allows the authenticating users to unfollow the user specified in the ID parameter.<br />
    * Returns the unfollowed user in the requested format when successful. Returns a string describing the failure condition when unsuccessful.
    * <br />This method calls twitter4j.Twitter.destroyFriendship.
-   * <br />destroyFriendship calls http://api.twitter.com/1/friendships/destroy/[id].json
+   * <br />destroyFriendship calls http://api.twitter.com/1.1/friendships/destroy/[id].json
    * <br />Note1: You must set parameter screenName or userId at least.
    * <br />Note2: Parameter userId is taken priority over screenName.
    *
@@ -90,7 +90,7 @@ trait FriendsFollowersResources {
    * @return User
    * @throws TwitterException when Twitter service or network is unavailable
    * @throws IllegalArgumentException when specificUser is set null.
-   * @see <a href="https://dev.twitter.com/docs/api/1/post/friendships/destroy">POST friendships/destroy | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/post/friendships/destroy">POST friendships/destroy | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def destroyFriendship(specificUser: User.SpecificInfo): User
@@ -98,7 +98,7 @@ trait FriendsFollowersResources {
   /**
    * Returns detailed information about the relationship between two users.
    * <br />This method calls twitter4j.Twitter.showFriendship
-   * <br />showFriendship calls http://api.twitter.com/1/friendships/show.json
+   * <br />showFriendship calls http://api.twitter.com/1.1/friendships/show.json
    * <br />Note1: You must set source and target ScreenName or source and target Id at least combination
    * <br />Note2: Id is taken priority over ScreenName.
    *
@@ -107,7 +107,7 @@ trait FriendsFollowersResources {
    * @return Relationship
    * @throws TwitterException when Twitter service or network is unavailable
    * @throws IllegalArgumentException when same information combination on source and target can not create.
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/friendships/show">GET friendships/show | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/friendships/show">GET friendships/show | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def showFriendship(
@@ -117,12 +117,12 @@ trait FriendsFollowersResources {
   /**
    * Returns an array of numeric IDs for every user who has a pending request to follow the authenticating user.
    * <br />This method calls twitter4j.Twitter.getIncomingFriendships.
-   * <br />getIncomingFriendships calls http://api.twitter.com/1/friendships/incoming.json
+   * <br />getIncomingFriendships calls http://api.twitter.com/1.1/friendships/incoming.json
    *
    * @param cursor Breaks the results into pages. A single page contains 5000 identifiers. Provide a value of -1 to begin paging.
    * @return an array of numeric IDs for every user who has a pending request to follow the authenticating user.
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/friendships/incoming">GET friendships/incoming | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/friendships/incoming">GET friendships/incoming | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def getIncomingFriendships(cursor: Long): IDs
@@ -130,12 +130,12 @@ trait FriendsFollowersResources {
   /**
    * Returns an array of numeric IDs for every protected user for whom the authenticating user has a pending follow request.
    * <br />This method calls twitter4j.Twitter.getOutgoingFriendships.
-   * <br />This method calls http://api.twitter.com/1/friendships/outgoing.json
+   * <br />This method calls http://api.twitter.com/1.1/friendships/outgoing.json
    *
    * @param cursor Breaks the results into pages. A single page contains 5000 identifiers. Provide a value of -1 to begin paging.
    * @return an array of numeric IDs for every protected user for whom the authenticating user has a pending follow request.
    * @throws TwitterException when Twitter service or network is unavailable
-   * @see <a href="https://dev.twitter.com/docs/api/1/get/friendships/outgoing">GET friendships/outgoing | Twitter Developers</a>
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/friendships/outgoing">GET friendships/outgoing | Twitter Developers</a>
    * @since Twitter4S 1.0.0
    */
   def getOutgoingFriendships(cursor: Long): IDs
@@ -144,7 +144,7 @@ trait FriendsFollowersResources {
    * Returns the relationship of the authenticating user to the specified users.
    * <br />This method has not been finalized and the interface is subject to change in incompatible ways.
    * <br />This method calls twitter4j.Twitter.lookupFriendships.
-   * <br />lookupFriendships calls http://api.twitter.com/1/friendships/lookup.json
+   * <br />lookupFriendships calls http://api.twitter.com/1.1/friendships/lookup.json
    * <br />Note1: You must set screenNames or ids at least.
    * <br />Note2: Parameter ids is taken priority over screenNames.
    *
@@ -160,7 +160,7 @@ trait FriendsFollowersResources {
    * Allows you to enable or disable retweets and device notifications from the specified user.
    * <br />This method has not been finalized and the interface is subject to change in incompatible ways.
    * <br />This method calls twitter4j.Twitter.lookupFriendships.
-   * <br />lookupFriendships calls http://api.twitter.com/1/friendships/update.json
+   * <br />lookupFriendships calls http://api.twitter.com/1.1/friendships/update.json
    * <br />Note1: You must set screenName or id at least.
    * <br />Note2: Parameter id is taken priority over screenName.
    *
@@ -178,7 +178,33 @@ trait FriendsFollowersResources {
       enableDeviceNotification: Boolean,
       retweets: Boolean): Relationship
   
+  /**
+   * Returns a cursored collection of user objects for every user the specified user is following (otherwise known as their "friends").<br>
+   * At this time, results are ordered with the most recent following first — however, this ordering is subject to unannounced change and eventual consistency issues. Results are given in groups of 20 users and multiple "pages" of results can be navigated through using the next_cursor value in subsequent requests. See <a href="https://dev.twitter.com/docs/misc/cursoring">Using cursors to navigate collections</a> for more information.
+   * <br />This method calls http://api.twitter.com/1.1/friends/list.json
+   * <br />This method calls twitter4j.Twitter.getFriendsList
+   *
+   * @param specificUser The ID or screen name of the user for whom to return results for.
+   * @param cursor Causes the results to be broken into pages of no more than 20 records at a time.
+   * @return list of friends
+   * @throws TwitterException when Twitter service or network is unavailable
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/friends/list">GET friends/list | Twitter Developers</a>
+   * @since Twitter4S 2.0.0
+   */
   def getFriendsList(specificUser: User.SpecificInfo, cursor: Long): PagableResponseList[twitter4j.User]
-  
+
+  /**
+   * Returns a cursored collection of user objects for users following the specified user.<br>
+   * At this time, results are ordered with the most recent following first — however, this ordering is subject to unannounced change and eventual consistency issues. Results are given in groups of 20 users and multiple "pages" of results can be navigated through using the next_cursor value in subsequent requests. See <a href="https://dev.twitter.com/docs/misc/cursoring">Using cursors to navigate collections</a> for more information.
+   * <br />This method calls http://api.twitter.com/1.1/friends/list.json
+   * <br />This method calls twitter4j.Twitter.getFollowersList
+   *
+   * @param specifidUser The ID or screen name of the user for whom to return results for.
+   * @param cursor Causes the results to be broken into pages of no more than 20 records at a time.
+   * @return list of followers
+   * @throws TwitterException when Twitter service or network is unavailable
+   * @see <a href="https://dev.twitter.com/docs/api/1.1/get/followers/list">GET followers/list | Twitter Developers</a>
+   * @since Twitter4S 2.0.0
+   */
   def getFollowersList(specificUser: User.SpecificInfo, cursor: Long): PagableResponseList[twitter4j.User]
 }
--- src/main/scala/twitter4s/api/impl/FriendsFollowersResourcesImpl.scala
+++ src/main/scala/twitter4s/api/impl/FriendsFollowersResourcesImpl.scala
@@ -1,5 +1,19 @@
 package twitter4s.api.impl
-
+/*
+ * Copyright (C) 2013 Shinsuke Abe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import twitter4s.IDs
 import twitter4s.api.FriendsFollowersResources
 import scala.util.Either
@@ -11,6 +25,10 @@ import twitter4j.Friendship
 import twitter4s.User
 import twitter4s.PagableResponseList
 
+/**
+ * @author Shinsuke Abe - mao.instantlife at gmail.com
+ * @since Twitter4S 2.0.0
+ */
 trait FriendsFollowersResourcesImpl extends FriendsFollowersResources {
   self: Twitter =>
     
@@ -129,6 +147,9 @@ trait FriendsFollowersResourcesImpl extends FriendsFollowersResources {
     }
   }
   
+  /**
+   * {@inheritDoc}
+   */
   def getFriendsList(specificUser: User.SpecificInfo, cursor: Long): PagableResponseList[twitter4j.User] = {
     require(specificUser != null)
     
@@ -138,6 +159,9 @@ trait FriendsFollowersResourcesImpl extends FriendsFollowersResources {
     }
   }
   
+  /**
+   * {@inheritDoc}
+   */
   def getFollowersList(specificUser: User.SpecificInfo, cursor: Long): PagableResponseList[twitter4j.User] = {
     require(specificUser != null)
     
