8360275886f3af6d8f99a384380d5c50144eebaf
Shinsuke Abe <mao.instantlife@gmail.com>
2013-09-07 21:40:22 +0900

    add lisence comment and remove unuse import.

--- src/main/scala/twitter4s/dsl/HelpResourcesDsl.scala
+++ src/main/scala/twitter4s/dsl/HelpResourcesDsl.scala
@@ -1,20 +1,32 @@
 package twitter4s.dsl
 
+/*
+ * Copyright (C) 2013 Shinsuke Abe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import twitter4s.{ResponseList, TwitterAPIConfiguration, Twitter}
 import twitter4s.api.impl.HelpResourcesImpl
 import twitter4s.auth.ConsumerKey
 import twitter4j.auth.AccessToken
 import twitter4s.api.HelpResources
-import twitter4j.{RateLimitStatus, TwitterException}
+import twitter4j.RateLimitStatus
 import twitter4j.api.HelpResources.Language
 import scala.collection.mutable
 
 /**
- * Created with IntelliJ IDEA.
- * User: mao
- * Date: 13/09/05
- * Time: 22:08
- * To change this template use File | Settings | File Templates.
+ * @author mao.instantlife at gmail.com
  */
 trait HelpResourcesDsl extends HelpResources {
 
