48ca80ca22983d80383ff44b13f5f14d98ac7660
Shinsuke Abe <mao.instantlife@gmail.com>
2013-09-08 21:26:39 +0900

    mocked twitter4j object in TimelineResources tests.

--- src/test/scala/twitter4s/TimelinesResourcesTest.scala
+++ src/test/scala/twitter4s/TimelinesResourcesTest.scala
@@ -1,14 +1,61 @@
 package twitter4s
-import twitter4s._
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 import Twitter4sTestHelper._
 import twitter4j.json.DataObjectFactory
 import twitter4s.api.impl.TimelinesResourcesImpl
+import org.specs2.mock.Mockito
+import java.util
 
 @RunWith(classOf[JUnitRunner])
-class TimelineMethodsTest extends Specification {
+class TimelinesResourcesTest extends Specification with Mockito {
+  val fakeReturnsResponseList = new twitter4j.ResponseList[twitter4j.Status] {
+    def getAccessLevel: Int = 3
+    def getRateLimitStatus: twitter4j.RateLimitStatus = ???
+    def size(): Int = 1
+    def isEmpty: Boolean = ???
+    def contains(o: scala.Any): Boolean = ???
+    def iterator(): util.Iterator[twitter4j.Status] = ???
+    def toArray[T](a: Array[T]): Array[T] = ???
+    def toArray: Array[AnyRef] = ???
+    def add(e: twitter4j.Status): Boolean = ???
+    def remove(o: scala.Any): Boolean = ???
+    def containsAll(c: util.Collection[_]): Boolean = ???
+    def addAll(c: util.Collection[_ <: twitter4j.Status]): Boolean = ???
+    def addAll(index: Int, c: util.Collection[_ <: twitter4j.Status]): Boolean = ???
+    def removeAll(c: util.Collection[_]): Boolean = ???
+    def retainAll(c: util.Collection[_]): Boolean = ???
+    def clear() {}
+    def get(index: Int): twitter4j.Status = ???
+    def set(index: Int, element: twitter4j.Status): twitter4j.Status = ???
+    def add(index: Int, element: twitter4j.Status) {}
+    def remove(index: Int): twitter4j.Status = ???
+    def indexOf(o: scala.Any): Int = ???
+    def lastIndexOf(o: scala.Any): Int = ???
+    def listIterator(): util.ListIterator[twitter4j.Status] = ???
+    def listIterator(index: Int): util.ListIterator[twitter4j.Status] = ???
+    def subList(fromIndex: Int, toIndex: Int): util.List[twitter4j.Status] = ???
+    def toArray[T](x: Array[T with Object]): Array[T with Object] = ???
+  }
+
+  val twitter4jMock = mock[twitter4j.Twitter]
+  twitter4jMock.getHomeTimeline returns fakeReturnsResponseList
+  twitter4jMock.getHomeTimeline(Paging(1)) returns fakeReturnsResponseList
+  twitter4jMock.getUserTimeline returns fakeReturnsResponseList
+  twitter4jMock.getUserTimeline(Paging(1).count(30)) returns fakeReturnsResponseList
+  twitter4jMock.getUserTimeline(1000) returns fakeReturnsResponseList
+  twitter4jMock.getUserTimeline(1001, Paging(sinceId = 999383469L)) returns fakeReturnsResponseList
+  twitter4jMock.getUserTimeline("1000") returns fakeReturnsResponseList
+  twitter4jMock.getUserTimeline("1001", Paging(1)) returns fakeReturnsResponseList
+  twitter4jMock.getMentions returns fakeReturnsResponseList
+  twitter4jMock.getMentions(Paging(1)) returns fakeReturnsResponseList
+  twitter4jMock.getMentionsTimeline returns fakeReturnsResponseList
+  twitter4jMock.getMentionsTimeline(Paging(1)) returns fakeReturnsResponseList
+  twitter4jMock.getRetweetsOfMe returns fakeReturnsResponseList
+  twitter4jMock.getRetweetsOfMe(Paging(9)) returns fakeReturnsResponseList
+
+  val twitter4sObj = new Twitter(twitter4jMock) with TimelinesResourcesImpl
   
   val twitterTimelineResourceRole = new Twitter(twitter4jInstance(User1)) with TimelinesResourcesImpl
   
@@ -17,110 +64,92 @@ class TimelineMethodsTest extends Specification {
     target(0) must equalTo(DataObjectFactory.createStatus(rawJSON(target(0))))
     target.size must be_>(0)
   }
+
+  def assertReturnedFakeResponseExactly(statuses: ResponseList[twitter4j.Status]) = {
+    statuses.accessLevel must equalTo(TwitterResponse.READ_WRITE_DIRECTMESSAGES)
+    statuses.size must equalTo(1)
+  }
   
   "getHomeTimeline" should {
     "get authenticated user's home timeline" in {
-      val statuses = twitterTimelineResourceRole.getHomeTimeline()
-      statuses.size must be_>(0)
-      twitterTimelineResourceRole.configuration.isJSONStoreEnabled must beTrue
-      rawJSON(statuses.tw4jObj) must not equalTo(null)
-      statuses(0) must equalTo(DataObjectFactory.createStatus(rawJSON(statuses(0))))
+      assertReturnedFakeResponseExactly(twitter4sObj.getHomeTimeline())
+      there was one(twitter4jMock).getHomeTimeline
     }
     
     "get authenticated user's home timeline with page" in {
-      val statuses = twitterTimelineResourceRole.getHomeTimeline(Paging(1))
-      rawJSON(statuses.tw4jObj) must not equalTo(null)
+      assertReturnedFakeResponseExactly(twitter4sObj.getHomeTimeline(Paging(1)))
+      there was one(twitter4jMock).getHomeTimeline(Paging(1))
     }
   }
   
   "getUserTimeline" should {
     "get user timeline no parameters" in {
-      val statuses = twitterTimelineResourceRole.getUserTimeline()
-      statuses.size must be_>(0)
+      assertReturnedFakeResponseExactly(twitter4sObj.getUserTimeline())
+      there was one(twitter4jMock).getUserTimeline
     }
     
     "get user timeline with screen name" in {
-      val statuses = twitterTimelineResourceRole.getUserTimeline(User.isSpecifiedBy("1000"))
-      testStatuses(statuses)
-      statuses(0).getUser().id must equalTo(9737332)
+      assertReturnedFakeResponseExactly(twitter4sObj.getUserTimeline(User.isSpecifiedBy("1000")))
+      there was one(twitter4jMock).getUserTimeline("1000")
+    }
+
+    "get user timeline with screen name and page number" in {
+      assertReturnedFakeResponseExactly(twitter4sObj.getUserTimeline(User.isSpecifiedBy("1001"), Paging(1)))
+      there was one(twitter4jMock).getUserTimeline("1001", Paging(1))
     }
     
     "get user timeline with user id" in {
-      val statuses = twitterTimelineResourceRole.getUserTimeline(User.isSpecifiedBy(1000))
-      testStatuses(statuses)
-      statuses(0).getUser.id must equalTo(1000)
+      assertReturnedFakeResponseExactly(twitter4sObj.getUserTimeline(User.isSpecifiedBy(1000)))
+      there was one(twitter4jMock).getUserTimeline(1000)
     }
-    
-    "get authed user's timeline specified sinceid" in {
-      val statuses = twitterTimelineResourceRole.getUserTimeline(paging = Paging(sinceId = 999383469L))
-      testStatuses(statuses)
+
+    "get user timeline with user id and specified sinceid" in {
+      assertReturnedFakeResponseExactly(twitter4sObj.getUserTimeline(User.isSpecifiedBy(1001), Paging(sinceId = 999383469L)))
+      there was one(twitter4jMock).getUserTimeline(1001, Paging(sinceId = 999383469L))
     }
     
     "get authed user's timeline specified page count" in {
-      val statuses = twitterTimelineResourceRole.getUserTimeline(paging = Paging(1).count(30))
-      testStatuses(statuses)
-    }
-    
-    "get users timeline with deffer page setting but same tweet count" in {
-      val statuses1 = twitterTimelineResourceRole.getUserTimeline(paging = Paging(1).count(30))
-      val statuses2 = twitterTimelineResourceRole.getUserTimeline(paging = Paging(2).count(15))
-      statuses1(statuses1.size - 1) must equalTo(statuses2(statuses2.size - 1))
+      assertReturnedFakeResponseExactly(twitter4sObj.getUserTimeline(paging = Paging(1).count(30)))
+      there was one(twitter4jMock).getUserTimeline(Paging(1).count(30))
     }
   }
   
   "getMentions" should {
-    // Note: getMentions is deprecated method from twitter4j 3.0.0
-    // The following specs are work.
-    // But comment out for rate limit status on run specs.
-    /*
-    "get specified user's mention list without parameter" in {
-      val statuses = twitter1.getMentions()
-      testStatuses(statuses)
+    // Note: getMentions is deprecated method since twitter4j 3.0.0
+    // The following specs are work now.
+
+    "get mentions timeline without pages" in {
+      assertReturnedFakeResponseExactly(twitter4sObj.getMentions())
+      there was one(twitter4jMock).getMentions
     }
-    
-    "get specified user's mention list with page parameter" in {
-      val statuses = twitter1.getMentions(Paging(1))
-      testStatuses(statuses)
+
+    "get mentions timeline with page" in {
+      assertReturnedFakeResponseExactly(twitter4sObj.getMentions(Paging(1)))
+      there was one(twitter4jMock).getMentions(Paging(1))
     }
-    
-    "get specified user's mention list with page and sinceId parameter" in {
-      val statuses = twitter1.getMentions(Paging(1, sinceId = 1L))
-      testStatuses(statuses)
-    }
-    
-    "get specified user's mention list with sinceId parameter" in {
-      val statuses = twitter1.getMentions(Paging(sinceId = 1L))
-      testStatuses(statuses)
-    }
-    */
   }
   
   "getMentionsTimeline" should {
     "get specified user's mention list without parameters" in {
-      val statuses = twitterTimelineResourceRole.getMentionsTimeline()
-      testStatuses(statuses)
+      assertReturnedFakeResponseExactly(twitter4sObj.getMentionsTimeline())
+      there was one(twitter4jMock).getMentionsTimeline
     }
     
     "get specified user's mention list with page parameter" in {
-      val statuses = twitterTimelineResourceRole.getMentionsTimeline(Paging(1))
-      testStatuses(statuses)
-    }
-    
-    "get specified user's mention list with page and sinceId parameter" in {
-      val statuses = twitterTimelineResourceRole.getMentionsTimeline(Paging(1, sinceId = 1L))
-      testStatuses(statuses)
-    }
-    
-    "get specified user's mention list with sinceId parameter" in {
-      val statuses = twitterTimelineResourceRole.getMentionsTimeline(Paging(sinceId = 1L))
-      testStatuses(statuses)
+      assertReturnedFakeResponseExactly(twitter4sObj.getMentionsTimeline(Paging(1)))
+      there was one(twitter4jMock).getMentionsTimeline(Paging(1))
     }
   }
   
   "getRetweetsOfMe" should {
-    "get retweets statuses of user" in {
-      val statuses = twitterTimelineResourceRole.getRetweetsOfMe()
-      statuses.size must be_>(0)
+    "get retweets statuses of user without parameters" in {
+      assertReturnedFakeResponseExactly(twitter4sObj.getRetweetsOfMe())
+      there was one(twitter4jMock).getRetweetsOfMe
+    }
+
+    "get retweets statuses of user with page parameter" in {
+      assertReturnedFakeResponseExactly(twitter4sObj.getRetweetsOfMe(Paging(9)))
+      there was one(twitter4jMock).getRetweetsOfMe(Paging(9))
     }
   }
 }
