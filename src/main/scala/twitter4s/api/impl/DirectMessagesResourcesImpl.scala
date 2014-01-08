0162621b332852218b25c8a7ecf2273c4fb82589
Shinsuke Abe <mao.instantlife@gmail.com>
2014-01-08 22:44:01 +0900

    optimized import and add header comment.

--- src/main/scala/twitter4s/api/impl/DirectMessagesResourcesImpl.scala
+++ src/main/scala/twitter4s/api/impl/DirectMessagesResourcesImpl.scala
@@ -1,14 +1,29 @@
 package twitter4s.api.impl
 
+/*
+ * Copyright (C) 2013 Shinsuke Abe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import twitter4s.ResponseList
-import twitter4j.Paging
 import twitter4s.api.DirectMessagesResources
-import scala.util.Either
 import twitter4s.Twitter
 import twitter4s.User
 import twitter4s.DirectMessage
 
-// TODO import整理
+/**
+ * @author mao.instantlife at gmail.com
+ */
 trait DirectMessagesResourcesImpl extends DirectMessagesResources {
   self: Twitter =>
     
