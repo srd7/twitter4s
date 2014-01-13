1f038fdf7cb9190f089b62d47f098fd352b7d201
Shinsuke Abe <mao.instantlife@gmail.com>
2014-01-13 18:00:28 +0900

    refactoring test and TwitterResponse.

--- src/main/scala/twitter4s/TwitterResponse.scala
+++ src/main/scala/twitter4s/TwitterResponse.scala
@@ -14,7 +14,6 @@ package twitter4s
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import twitter4j.TwitterException
 
 /**
  * @author Shinsuke Abe - mao.instantlife at gmail.com
--- src/test/scala/twitter4s/TwitterTest.scala
+++ src/test/scala/twitter4s/TwitterTest.scala
@@ -1,8 +1,6 @@
 package twitter4s
-import twitter4s._
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
-import Twitter4sTestHelper.id1
 import Twitter4sTestHelper.prop
 import Twitter4sTestHelper.rawJSON
 import Twitter4sTestHelper.rwPrivateMessage
@@ -11,30 +9,40 @@ import auth.ConsumerKey
 import twitter4j.auth.AccessToken
 import twitter4j.json.DataObjectFactory
 import org.specs2.runner.JUnitRunner
+import org.specs2.mock.Mockito
+import twitter4s.mocked.FakeValuesUsedByMock
 
 @RunWith(classOf[JUnitRunner])
-class TwitterTest extends Specification {
+class TwitterTest extends Specification with Mockito {
+  // TODO TwitterResourcesTestBaseに集約(TargetResourcesTypeの削除が終わったら)
+  val mockedTwitter4j = mock[twitter4j.Twitter]
+
+  mockedTwitter4j.getScreenName returns(FakeValuesUsedByMock.userName)
+  mockedTwitter4j.getId returns(10L)
+
+  val twitter = new Twitter(mockedTwitter4j)
+
   "getAccessLevel" should {
     "application has read and write access level is READ_WRITE" in {
-      val response = twitter1.verifyCredentials
-      response.accessLevel must equalTo(TwitterResponse.READ_WRITE)
+      twitter1.verifyCredentials.accessLevel must equalTo(TwitterResponse.READ_WRITE)
     }
   
     "application has all access level is READ_WRITE_DIRECTMESSAGES" in {
-      val response = rwPrivateMessage.verifyCredentials
-      response.accessLevel must equalTo(TwitterResponse.READ_WRITE_DIRECTMESSAGES)
+      rwPrivateMessage.verifyCredentials.accessLevel must equalTo(TwitterResponse.READ_WRITE_DIRECTMESSAGES)
     }
   }
   
   "screenName" should {
-    "get authorized user's screen name" in {
-      twitter1.screenName must equalTo(id1.screenName)
+    "call twitter4j getScreenName method" in {
+      twitter.screenName must equalTo(FakeValuesUsedByMock.userName)
+      there was one(mockedTwitter4j).getScreenName
     }
   }
   
   "id" should {
     "get authorized user's id" in {
-      twitter1.id must equalTo(id1.id)
+      twitter.id must equalTo(10L)
+      there was one(mockedTwitter4j).getId
     }
   }
   
@@ -68,34 +76,6 @@ class TwitterTest extends Specification {
       val user = twitterObj.verifyCredentials
       rawJSON(user.tw4jObj) must not equalTo(null)
       user.tw4jObj must equalTo(DataObjectFactory.createUser(rawJSON(user.tw4jObj)))
-    }
-  }
-
-  // TODO mock化
-  "getRateLimitStatus" should {
-    "get rate limit status" in {
-      val rateLimitStatus = twitter1.getRateLimitStatus()
-      DataObjectFactory.getRawJSON(rateLimitStatus) must not equalTo(null)
-      rateLimitStatus must equalTo(DataObjectFactory.createRateLimitStatus(rawJSON(rateLimitStatus)))
-      
-      val status = rateLimitStatus.values.iterator.next
-      status.limit must be_>(10)
-      status.remaining must be_>(10)
-      status.secondsUntilReset must be_>(0)
-    }
-    
-    "get comparable rate limit status" in {
-      twitter1.getMentions()
-      val previousRateLimitStatus = twitter1.getRateLimitStatus()
-      val previousStatus = previousRateLimitStatus.values.iterator.next
-      
-      twitter1.getMentions()
-      val afterRateLimitStatus = twitter1.getRateLimitStatus()
-      val afterStatus = afterRateLimitStatus.values.iterator.next
-      
-      previousStatus.remaining must be_>(afterStatus.remaining)
-      previousStatus.limit must equalTo(afterStatus.limit)
-      true
     }
   }
   
